a58ffd67e1090befef3b4761e1946e7b9af240cd
tuchi <edu@192.168.11.21>
2025-11-17 10:33:38 +0900

    11/17

--- morning_practice_for_req-train/src/day03/Lesson5.java
+++ morning_practice_for_req-train/src/day03/Lesson5.java
@@ -21,12 +21,40 @@ input number[0or1]? >>2
 */
 package day03;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Lesson5 {
 
 	public static void main(String[] args) throws IOException {
 		/*ここから記入*/
+		boolean[] number ;
+		number = new boolean[2];
+		number[0] = true;
+		number[1] = false;
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String inputNum = reader.readLine();
+		int iNum = Integer.parseInt(inputNum);
+		
+		System.out.println("input number[0 or1]?。");
+		
+		switch (iNum) {
+		case 0:
+			System.out.println("配列の"+iNum+"番目は"+number[iNum]+"です");
+			break;
+			
+		case 1:
+			System.out.println("配列の"+iNum+"番目は"+number[iNum]+"です");
+			break;
+			
+		default:
+			System.out.println("配列の範囲外の値が入力されました");
+			break;
+		
+		}
+		
 
 	}
 
